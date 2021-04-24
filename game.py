156be26a17dac91655499238f730b3cb26be7517
Chris Farfan <chrisfarfandelosgodos@gmail.com>
2021-04-24 16:49:44 +0100

    Added txt import for weapons & example

--- game.py
+++ game.py
@@ -6,6 +6,15 @@ from random import sample
 
     # Set Working directory
 
+chdir(path.dirname(__file__))
+
+    # import data from .txt into arrays
+    # var_name = (open("txt_file_name.txt","r").readlines())[0].split(","))
+
+adjective = (open("weapon_adjectives.txt", "r").readlines())[0].split(",")
+noun1 = (open("weapon_nouns1.txt", "r").readlines())[0].split(",")
+noun2 = (open("weapon_nouns2.txt", "r").readlines())[0].split(",")
+
 # Functions
 
 # Main block
