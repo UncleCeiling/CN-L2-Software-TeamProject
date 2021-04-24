a313f6ac1b423d751c02d5c8533f1b58100009ba
Chris Farfan <chrisfarfandelosgodos@gmail.com>
2021-04-24 18:46:52 +0100

    Added gen_weapon() def

--- game.py
+++ game.py
@@ -17,6 +17,17 @@ noun2 = (open("weapon_nouns2.txt", "r").readlines())[0].split(",")
 
 # Functions
 
+def gen_weapon(): # Simple Weapon Generator (adjective noun of noun2)
+    sample_adjective = sample(adjective,1)[0]
+    print(sample_adjective)
+    sample_noun1 = sample(noun1,1)[0]
+    print(sample_noun1)
+    sample_noun2 = sample(noun2,1)[0]
+    print(sample_noun2)
+    return (f"{sample_adjective} {sample_noun1} of {sample_noun2}")
+
+
+
 # Main block
 
 # Hi Chris
