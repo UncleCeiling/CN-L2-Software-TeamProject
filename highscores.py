b058981055a51a547f5f68b2e6afe96221925cc7
Chris Farfan de los Godos <83005088+UncleCeiling@users.noreply.github.com>
2021-04-26 12:51:43 +0100

    Made into a function

--- highscores.py
+++ highscores.py
@@ -1,18 +1,15 @@
-import random
-
 # import data from .txt file read first line only print in sorted order highest to lowest
 
-score = 0
+def highscore():
+    file=open(highscores.txt, "r",)
+    for i in range(0, 2):
+        name = file.readlines(int+str)
+        file.read(str(score)+name+"\n")
+        file.close()
 
-file=open(highscores.txt, "r",)
-for i in range(0, 2):
-    name = file.readlines(int+str)
-    file.read(str(score)+name+"\n")
-    file.close()
+    file = open("highscores.txt","r")
+    readthefile = file.readlines()
+    sortedData = sorted(readthefile,reverse= True)
 
-file = open("highscores.txt","r")
-readthefile = file.readlines()
-sortedData = sorted(readthefile,reverse= True)
-
-print("top 3 scores!")
-print("pos\tpoints, Name")
+    print("top 3 scores!")
+    print("pos\tpoints, Name")
