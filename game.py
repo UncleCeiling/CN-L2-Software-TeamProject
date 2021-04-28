6c53bc01eb2ac5d57df933823f945d68a3b716e7
CodeMD96 <70902741+CodeMD96@users.noreply.github.com>
2021-04-28 12:27:32 +0100

    Update game.py

--- game.py
+++ game.py
@@ -224,3 +224,336 @@ else:
     print("You play the game") # PLACEHOLDER
 
 # TEMP
+from os import chdir, path, scandir, walk                            # To set Working Directory
+from random import sample, randint, shuffle                              # For sampling lists
+
+
+chdir(path.dirname(__file__))
+
+
+def room_selecter():
+    combat_room = (open(path.join("storage", "roomscombat.txt"),"r").readlines())[0].split(",")
+    puzzle_room = (open(path.join("storage", "roomspuzzle.txt"), "r").readlines())[0].split(",")
+    combat_samples = randint(0, 3)   # Generates a random int from 0-3.
+    options = sample(combat_room, combat_samples) + sample(puzzle_room, 3 - combat_samples) # Creates a list of 3 randomised strings from roomscombat.txt and roomspuzzle.txt.
+    shuffle(options)   # Shuffles the list so they aren't always in combat-puzzle order. 
+
+    print(f"""There are 3 rooms before you:
+    Door 1: {options[0]}
+    Door 2: {options[1]}
+    Door 3: {options[2]}""")
+    input_var = (input("Please choose a door (A, B or C)"))[0].lower()
+    while input_var not in ["a","b","c"]:
+        input_var = (input("For real this time, pick an option from A, B or C : "))[0].lower()
+    if input_var == "a":
+        print("You open door A")
+        if options[0] in (open(path.join("storage", "roomscombat.txt"),"r").readlines())[0].split(","):
+            return True
+        else:
+            return False
+    elif input_var == "b":
+        print("You open door B")
+        if options[1] in (open(path.join("storage", "roomscombat.txt"),"r").readlines())[0].split(","):
+            return True
+        else:
+            return False
+    else:
+        print("You open door C")
+        if options[2] in (open(path.join("storage", "roomscombat.txt"),"r").readlines())[0].split(","):
+            return True
+        else:
+            return False
+# You choose between doors A, B and C and the function returns true if that door is a combat room and false if it is a puzzle room.
+
+# room_selecter()
+
+def room_generator():
+    global combat_rooms
+    global puzzle_rooms
+    room_type =room_selecter()
+    if room_type == True:
+        combat_rooms += 1
+        print("It's a combat room!")
+        # Code to call a random combat???
+    else:
+        puzzle_rooms += 1
+        print("It's a puzzle room!")
+        # Code to call a random puzzle???
+
+room_generator()
+
+from os import chdir, path, scandir, walk                            # To set Working Directory
+from random import sample, randint, shuffle                              # For sampling lists
+
+
+chdir(path.dirname(__file__))
+
+
+def room_selecter():
+    combat_room = (open(path.join("storage", "roomscombat.txt"),"r").readlines())[0].split(",")
+    puzzle_room = (open(path.join("storage", "roomspuzzle.txt"), "r").readlines())[0].split(",")
+    combat_samples = randint(0, 3)   # Generates a random int from 0-3.
+    options = sample(combat_room, combat_samples) + sample(puzzle_room, 3 - combat_samples) # Creates a list of 3 randomised strings from roomscombat.txt and roomspuzzle.txt.
+    shuffle(options)   # Shuffles the list so they aren't always in combat-puzzle order. 
+
+    print(f"""There are 3 rooms before you:
+    Door 1: {options[0]}
+    Door 2: {options[1]}
+    Door 3: {options[2]}""")
+    input_var = (input("Please choose a door (A, B or C)"))[0].lower()
+    while input_var not in ["a","b","c"]:
+        input_var = (input("For real this time, pick an option from A, B or C : "))[0].lower()
+    if input_var == "a":
+        print("You open door A")
+        if options[0] in (open(path.join("storage", "roomscombat.txt"),"r").readlines())[0].split(","):
+            return True
+        else:
+            return False
+    elif input_var == "b":
+        print("You open door B")
+        if options[1] in (open(path.join("storage", "roomscombat.txt"),"r").readlines())[0].split(","):
+            return True
+        else:
+            return False
+    else:
+        print("You open door C")
+        if options[2] in (open(path.join("storage", "roomscombat.txt"),"r").readlines())[0].split(","):
+            return True
+        else:
+            return False
+# You choose between doors A, B and C and the function returns true if that door is a combat room and false if it is a puzzle room.
+
+# room_selecter()
+
+def room_generator():
+    global combat_rooms
+    global puzzle_rooms
+    room_type =room_selecter()
+    if room_type == True:
+        combat_rooms += 1
+        print("It's a combat room!")
+        # Code to call a random combat???
+    else:
+        puzzle_rooms += 1
+        print("It's a puzzle room!")
+        # Code to call a random puzzle???
+
+room_generator()
+
+import random as r
+# Monty Hall problem
+def monty_hall():
+    a = ["a", "A"]
+    b = ["b", "B"]
+    c = ["c", "C"]
+    yes = ["Y", "y", "Yes", "yes", "YES"]
+    no = ["N", "n", "No", "no", "NO"]
+    invalid_ans = ("\nPlease use a valid input!\n")
+    doors = ["trap", "trap", "prize"]
+    ongoing = True
+
+    r.shuffle(doors)
+    # print(doors) #Debug code
+
+    print("Pick a door(a, b or c)")
+    choice_1 = input(">>>").lower()
+    if choice_1 in a and ongoing == True or choice_1 in b and ongoing == True or choice_1 in c and ongoing == True:
+        while choice_1 in a and ongoing == True:
+            if doors[0] == "trap":
+                print("Door b is a trap door! Do you want to change to door c? (y/n)")
+                choice_2 = input(">>>")
+                if choice_2 in yes:
+                    ongoing = False
+                    choice_1 = "c"
+                elif choice_2 in no:
+                    ongoing = False
+                else:
+                    print(invalid_ans)
+            elif doors[2] == "trap":
+                print("Door c is a trap door! Do you want to change to door b? (y/n)")
+                choice_2 = input(">>>")
+                if choice_2 in yes:
+                    ongoing = False
+                    choice_1 = "b"
+                elif choice_2 in no:
+                    ongoing = False
+                else:
+                    print(invalid_ans)
+        while choice_1 in b and ongoing == True:
+            if doors[0] == "trap":
+                print("Door a is a trap door! Do you want to change to door c? (y/n)")
+                choice_2 = input(">>>")
+                if choice_2 in yes:
+                    ongoing = False
+                    choice_1 = "c"
+                elif choice_2 in no:
+                    ongoing = False
+                else:
+                    print(invalid_ans)
+            elif doors[2] == "trap":
+                print("Door c is a trap door! Do you want to change to door a? (y/n)")
+                choice_2 = input(">>>")
+                if choice_2 in yes:
+                    ongoing = False
+                    choice_1 = "a"
+                elif choice_2 in no:
+                    ongoing = False
+                else:
+                    print(invalid_ans)
+        while choice_1 in c and ongoing == True:
+            if doors[0] == "trap":
+                print("Door a is a trap door! Do you want to change to door b? (y/n)")
+                choice_2 = input(">>>")
+                if choice_2 in yes:
+                    ongoing = False
+                    choice_1 = "b"
+                elif choice_2 in no:
+                    ongoing = False
+                else:
+                    print(invalid_ans)
+            elif doors[1] == "trap":
+                print("Door b is a trap door! Do you want to change to door a? (y/n)")
+                choice_2 = input(">>>")
+                if choice_2 in yes:
+                    ongoing = False
+                    choice_1 = "a"
+                elif choice_2 in no:
+                    ongoing = False
+                else:
+                    print(invalid_ans)
+
+    else:
+        print(invalid_ans)
+        monty_hall()
+
+    if choice_1 == "a":
+        print(f"What's behind door {choice_1}?")
+        print(f"{doors[0]}")
+    elif choice_1 == "b":
+        print(f"What's behind door {choice_1}?")
+        print(f"{doors[1]}")
+    elif choice_1 == "c":
+        print(f"What's behind door {choice_1}?")
+        print(f"{doors[2]}")
+
+import time
+import random as r
+import math
+
+battle_choice = "Attack, talk or run?"
+player_health = 9001                              # Player hitpoints, when this reaches 0 you lose.
+weapon = ["I want twenty attack plz", 20]         # Your weapon name, weapon points =len() of this string (- white space maybe?) 
+armour = ["Okay Armour", 10]                      # Your armour name, armour points =len() of this string (- white space maybe?) 
+damage_dealt = 0                                  # Logs how much damage player has dealt for scoring purposes
+damage_taken = 0                                  # Logs how much damage player has taken for scoring purposes
+
+def death_screen():
+    print("You died lol")
+
+def player_attack():
+    global enemy_health
+    global player_damage
+    global enemy_defence_p
+    global damage_dealt
+    player_damage = r.randint(int(round(weapon[1]-weapon[1]/2)), weapon[1])
+    enemy_block = r.randint(int(round(enemy_defence_p-enemy_defence_p/2)), enemy_defence_p)
+    damage = player_damage - enemy_block
+    
+    if damage > 0:
+        enemy_health = enemy_health - damage
+        damage_dealt = damage_dealt + damage
+        print(f"You attacked for {player_damage} damage!")
+        time.sleep(0.5)
+        print(f"{enemy_name} blocked {enemy_block}!")
+        time.sleep(0.5)
+        print(f"You dealt {damage} damage to the enemy!")
+    else:
+        print(f"You attacked {enemy_name} for {player_damage} damage!")
+        time.sleep(0.5)
+        print(f"{enemy_name} blocked {enemy_block}!")
+        time.sleep(0.5)
+        print(f"You dealt no damage to {enemy_name}!")
+# Function for player attacking the enemy.
+
+def enemy_attack():
+    global player_health
+    global enemy_damage
+    global damage_taken
+    enemy_damage = r.randint(int(round(enemy_attack_p-enemy_attack_p/2)), enemy_attack_p)
+    player_block = r.randint(int(round(armour[1]-armour[1]/2)), armour[1])
+    damage = enemy_damage - player_block
+    
+    if damage > 0:
+        player_health = player_health - damage
+        damage_taken = damage_taken + damage
+        print(f"{enemy_name} attacked for {enemy_damage} damage!")
+        time.sleep(0.5)
+        print(f"You blocked {player_block}!")
+        time.sleep(0.5)
+        print(f"{enemy_name} did {damage} damage to you!")
+        if player_health < 0:
+            death_screen()
+    else:
+        print(f"{enemy_name} attacked for {enemy_damage} damage!")
+        time.sleep(0.5)
+        print(f"You blocked {player_block}!")
+        time.sleep(0.5)
+        print(f"You blocked all the damage!")
+# Function for basic enemy attack, although more complex attacks can be made for specific enemies. See Mathemagician for example
+
+def enemy_1():
+    global enemy_name
+    global enemy_health
+    global enemy_attack_p
+    global enemy_defence_p
+    global player_defence_p
+    global player_health
+    global player_health
+    global enemy_damage
+    global player_block
+    global kill_count
+    combat = True
+    dead = False
+    battle = True
+    dead = False
+    enemy_name = "The enemy"
+    enemy_health = 100
+    enemy_attack_p = 20
+    enemy_defence_p = 2
+    print(f"An {enemy_name} appears! What do you do?")
+    while battle == True:
+        input_var = (input("Attack, talk or run?"))[0].lower()
+        while input_var not in ["a","t", "r"]:
+            input_var = (input("For real this time, pick an option from attack, talk or run : "))[0].lower()
+        if input_var == "a":
+            player_attack()
+            time.sleep(0.5)
+            if enemy_health > 0:
+                print(f"***{enemy_name}'s turn***")
+                enemy_attack()
+            else:
+                battle = False
+                dead = True
+        elif input_var == "t":
+            print("You try making smalltalk with the enemy.")
+            time.sleep(0.5)
+            print("...")
+            time.sleep(0.5)
+            print("The enemy ignores you.")
+            time.sleep(0.5)
+        else:
+            battle = False
+    if dead == True:
+        kill_count += 1
+        print("You killed the enemy!")
+        time.sleep(0.5)
+        print(f"You have {player_health} hit points left.")
+    else:
+        print(f"You casually walk past {enemy_name}")
+        time.sleep(0.5)
+        print(f"{enemy_name} watches you leave.")
+        time.sleep(0.5)
+        print(f"You managed to escape {enemy_name}!")
+    print(f"You have dealt {damage_dealt} damage.")    
+    print(f"You have taken {damage_taken} damage.")    
+# A basic enemy/ template for combats.
